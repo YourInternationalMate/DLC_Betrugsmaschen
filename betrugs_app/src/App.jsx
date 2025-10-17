365b673514168c32d6b94ac9f9036993cbdcf905
Mika Koenigsmann <73663569+YourInternationalMate@users.noreply.github.com>
2025-10-17 19:26:25 +0200

    Drawer eingebaut

--- betrugs_app/src/App.jsx
+++ betrugs_app/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Navigation from "./components/navigation/Navigation";
 
 function App() {
   return (
-    <div>
-      <h1>BETRUGSMASCHEN<br />IM INTERNET.</h1>
+    <div className="app-shell">
+      <Navigation />
+      <main>
+        <h1>BETRUGSMASCHEN<br />IM INTERNET.</h1>
+      </main>
     </div>
   );
 }
--- betrugs_app/src/components/navigation/Navigation.jsx
+++ betrugs_app/src/components/navigation/Navigation.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
+import {
+  AppBar, Toolbar, IconButton, Typography,
+  Drawer, List, ListItemButton, ListItemText
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import "./Navigation.css";
 
 function Navigation() {
   const [theme, setTheme] = useState("light");
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
@@ -13,11 +19,20 @@ function Navigation() {
   };
 
   return (
-    <div>
-      <button onClick={toggleTheme}>
-        Theme wechseln
-      </button>
-    </div>
+    <>
+      <IconButton edge="start" onClick={() => setOpen(true)} aria-label="Menu" >
+        <MenuIcon />
+      </IconButton>
+
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)} slotProps={{ paper: { className: "drawerPaper" } }}>
+        <List sx={{ width: 280 }} onClick={() => setOpen(false)}>
+          <ListItemButton component="a" href="#"><ListItemText primary="Einführung" /></ListItemButton>
+          <ListItemButton component="a" href="#"><ListItemText primary="Test" /></ListItemButton>
+          <ListItemButton component="a" href="#"><ListItemText primary="Test" /></ListItemButton>
+          <ListItemButton onClick={toggleTheme}><ListItemText primary="Theme" /></ListItemButton>
+        </List>
+      </Drawer>
+    </>
   );
 }
 
