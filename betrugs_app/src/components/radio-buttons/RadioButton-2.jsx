8b80349dbc0a580a87c8651b47ab1043b30a9fb8
Mika Koenigsmann <73663569+YourInternationalMate@users.noreply.github.com>
2025-10-24 10:05:06 +0200

    RadioButtons in 3 Varianten erstellt

---
+++ betrugs_app/src/components/radio-buttons/RadioButton-2.jsx
@@ -0,0 +1,23 @@
+import "./RadioButton.css";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+
+function  RadioButton2({value1, value2, handleSubmit}) {
+  return (
+    <div className="radio-btn-container">
+        <form onSubmit={handleSubmit}>
+            <FormControl className="form-container">
+                <RadioGroup name="radio-btn-group" className="radio-btn">
+                    <FormControlLabel value={value1} control={<Radio />} label={value1} />
+                    <FormControlLabel value={value2} control={<Radio />} label={value2} />
+                </RadioGroup>
+                <button type="submit" className="submit-btn">✓</button>
+            </FormControl>
+        </form>
+    </div>
+  );
+}
+
+export default RadioButton2;
