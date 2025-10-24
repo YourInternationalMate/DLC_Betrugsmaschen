f28d7a3b179af67d12f9f6df517f9c60a24130cb
Mika Koenigsmann <73663569+YourInternationalMate@users.noreply.github.com>
2025-10-24 10:32:15 +0200

    MultipleChoiceQuiz strukturiert und Funktionen hinzugefügt

--- betrugs_app/src/components/multiple-choice/MultipleChoiceQuiz.jsx
+++ betrugs_app/src/components/multiple-choice/MultipleChoiceQuiz.jsx
@@ -1,18 +1,71 @@
-import "./MultipleChoiceQuiz.css";
+import { useState } from "react";
+import RadioButton2 from "../radio-buttons/RadioButton-2";
+import RadioButton3 from "../radio-buttons/RadioButton-3";
+import RadioButton4 from "../radio-buttons/RadioButton-4";
 
-function MultipleChoiceQuiz() {
+function MultipleChoiceQuiz({ variant, value1, value2, value3, value4 }) {
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Ausgewählte Antwort:", selectedValue); // TODO: hier dann Bewertung
+  };
+
+  let content;
+
+  switch (variant) {
+    case 2:
+      content = (
+        <RadioButton2
+          value1={value1}
+          value2={value2}
+          handleSubmit={handleSubmit}
+          onChange={handleChange}
+          selectedValue={selectedValue}
+        />
+      );
+      break;
+    case 3:
+      content = (
+        <RadioButton3
+          value1={value1}
+          value2={value2}
+          value3={value3}
+          handleSubmit={handleSubmit}
+          onChange={handleChange}
+          selectedValue={selectedValue}
+        />
+      );
+      break;
+    case 4:
+      content = (
+        <RadioButton4
+          value1={value1}
+          value2={value2}
+          value3={value3}
+          value4={value4}
+          handleSubmit={handleSubmit}
+          onChange={handleChange}
+          selectedValue={selectedValue}
+        />
+      );
+      break;
+    default:
+      content = null;
+  }
+
   return (
     <div className="quiz-container">
-        <div className="video-conatiner">
-            {/* TODO: Video Component einfügen */}
-        </div>
-        <div>
-            {/* 2 - 4 Radio Buttons */}
-        </div>
-      
+      <div className="video-conatiner">
+        {/* TODO: Video Component einfügen */}
+      </div>
+      <div className="radio-btn-container">{content}</div>
     </div>
   );
 }
 
 export default MultipleChoiceQuiz;
- 
--- betrugs_app/src/components/radio-buttons/RadioButton-2.jsx
+++ betrugs_app/src/components/radio-buttons/RadioButton-2.jsx
@@ -1,21 +1,42 @@
 import "./RadioButton.css";
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 
-function  RadioButton2({value1, value2, handleSubmit}) {
+function RadioButton2({
+  value1,
+  value2,
+  handleSubmit,
+  onChange,
+  selectedValue,
+}) {
   return (
     <div className="radio-btn-container">
-        <form onSubmit={handleSubmit}>
-            <FormControl className="form-container">
-                <RadioGroup name="radio-btn-group" className="radio-btn">
-                    <FormControlLabel value={value1} control={<Radio />} label={value1} />
-                    <FormControlLabel value={value2} control={<Radio />} label={value2} />
-                </RadioGroup>
-                <button type="submit" className="submit-btn">✓</button>
-            </FormControl>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <FormControl className="form-container">
+          <RadioGroup
+            name="radio-btn-group"
+            className="radio-btn"
+            onChange={onChange}
+            value={selectedValue}
+          >
+            <FormControlLabel
+              value={value1}
+              control={<Radio />}
+              label={value1}
+            />
+            <FormControlLabel
+              value={value2}
+              control={<Radio />}
+              label={value2}
+            />
+          </RadioGroup>
+          <button type="submit" className="submit-btn">
+            ✓
+          </button>
+        </FormControl>
+      </form>
     </div>
   );
 }
