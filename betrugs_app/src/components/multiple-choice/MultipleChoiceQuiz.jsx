5cd30ce2b898d4f146f68ae8675bffb444510657
Mika Koenigsmann <73663569+YourInternationalMate@users.noreply.github.com>
2025-10-24 10:05:06 +0200

    RadioButtons in 3 Varianten erstellt

---
+++ betrugs_app/src/components/multiple-choice/MultipleChoiceQuiz.jsx
@@ -0,0 +1,18 @@
+import "./MultipleChoiceQuiz.css";
+
+function MultipleChoiceQuiz() {
+  return (
+    <div className="quiz-container">
+        <div className="video-conatiner">
+            {/* TODO: Video Component einfügen */}
+        </div>
+        <div>
+            {/* 2 - 4 Radio Buttons */}
+        </div>
+      
+    </div>
+  );
+}
+
+export default MultipleChoiceQuiz;
+ 
