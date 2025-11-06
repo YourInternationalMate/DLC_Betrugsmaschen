fab209223849385186057bb3d1d0bfc47949d1bb
Pudian <christian.pudimat@bmiag.de>
2025-11-06 08:36:42 +0100

    Video Komponente aus App.jsx gelöscht

--- betrugs_app/src/App.jsx
+++ betrugs_app/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import VideoPlayer from "./components/Video/Video";
 
 function App() {
   return (
     <div className="app-shell">
       <main>
         <h1>BETRUGSMASCHEN<br />IM INTERNET.</h1>
-          <VideoPlayer />
       </main>
     </div>
   );
